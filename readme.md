e9e8bbd2041aa326dd4a19a5ce09bb65d9bb5da8
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2022-07-06 15:05:04 -0400

    add id_number matches

--- readme.md
+++ readme.md
@@ -27,8 +27,10 @@ The OFAC dataset is larger than GBR and contains more record types:
 
 ## Approach
 
-Identify fields that make sense as identifiers and use them to compare lists. 
+My approach is to identify fields that make sense as identifiers and use them to compare lists. After investigating the data, I've determined that comparing `id_number`, `name` and `addresses` will be a reliable way to identify records in both datasets that refer to the same individual or entity. The `aliases` array may also be useful. I've decided not to use `reported_dates_of_birth` or other fields due to a risk of false positives. 
 
 ## Notes
 
  * Some names appear more than once within both datasets. Since the duplicates all appear to refer to the same individual or entity, I decided not to exclude them from the results. 
+ * Even though GBR and OFAC use different language for ID number comments, matching on only ID number values is very accurate. 
+ * The addresses in OFAC have a problem with extra commas that need to be handled. 
--- sanctioned_uk_ofac.sql
+++ sanctioned_uk_ofac.sql
@@ -1,15 +1,56 @@
 
--- name - 492 records match
+with
+-- get records with matching names
+name_match as (
+    select
+        ofac.id as ofac_id,
+        gbr.id as gbr_id,
+        "name match" as reason
+    from ofac
+    join gbr
+        on ofac.name = gbr.name
+),
+-- get records with matching ID numbers
+ofac_id_numbers as (
+    select
+        id,
+        explode(id_numbers) as id_number
+    from ofac
+),
+gbr_id_numbers as (
+    select
+        id,
+        explode(id_numbers) as id_number
+    from gbr
+),
+id_number_match as (
+    select
+        gbr.id as gbr_id,
+        ofac.id as ofac_id,
+        "id number match" as reason
+    from gbr_id_numbers as gbr
+    join ofac_id_numbers as ofac
+        on gbr.id_number.value = ofac.id_number.value
+),
+-- union the different matches
+all_matches as (
+    select
+        gbr_id,
+        ofac_id,
+        reason
+    from id_number_match
+    union
+    select
+        gbr_id,
+        ofac_id,
+        reason
+    from name_match
+)
 select
-    ofac.id as ofac_id,
-    gbr.id as gbr_id,
-    "name match" as reason
-from ofac
-join gbr
-    on ofac.name = gbr.name
-
--- addresses
--- id numbers
--- aliases
--- position?
--- date of birth?
+    gbr_id,
+    ofac_id,
+    concat_ws(',', collect_set(reason)) as reason
+from all_matches
+group by
+    gbr_id,
+    ofac_id
